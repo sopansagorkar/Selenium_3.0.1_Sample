91eb6c21b8df08e332567a8b48a9f433323432b8
Sopan sagorkar <sopansa@cybage.com>
2017-09-08 19:15:51 +0530

    Locater changed for username

--- src/test/java/com/cybage/test/TestClass.java
+++ src/test/java/com/cybage/test/TestClass.java
@@ -2,7 +2,6 @@ package com.cybage.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -20,12 +19,13 @@ public class TestClass {
 	@Test
 	public void firstTest() {
 		driver.get("http://newtours.demoaut.com");
-		driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input")).sendKeys("sopan");
+		driver.findElement(By.name("userName")).sendKeys("sopan");
 		driver.findElement(By.name("password")).sendKeys("sopan");
 		driver.findElement(By.name("login")).click();
 	}
+
 	@AfterTest
-	public void tearDown(){
+	public void tearDown() {
 		driver.quit();
 	}
 }
